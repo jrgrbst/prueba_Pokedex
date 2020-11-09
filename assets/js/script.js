84a8d6a27b3226f1280779a659d8dcd93dad3109
jrgrbst <j.rubiosoto@gmail.com>
2020-11-09 20:19:44 -0300

    avance con grafico listo, sumando más características

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@ $('#pokesearch').on('click',()=>{
     let entrada = $('input').val();
     console.log(entrada);
 
-
-
     $.ajax({
         type: "GET",
         url: `https://pokeapi.co/api/v2/pokemon/${entrada}`,
@@ -30,7 +28,12 @@ $('#pokesearch').on('click',()=>{
             <img src="${infoPoke.sprites.back_shiny}" alt="${infoPoke.name} shiny">
             `);
 
+            $('#pokeType > #info > p').html(`<h5>Tipo: </h5><p>${infoPoke.types[0]}</p>`);
+
+            $('#pokeExp > #info > p').html(`<h5>Experiencia Base: </h5><p>${infoPoke.base_experience}</p>`);
+
             $('#pokeAbility > #title > h2').html("Habilidades");
+
             $('#pokeAbility > #lista > ul').html("");
             infoPoke.abilities.forEach((habilidad,index) => {
                 $('#pokeAbility > #lista > ul').append(`<li>${habilidad.ability.name}</li>`);
@@ -44,30 +47,56 @@ $('#pokesearch').on('click',()=>{
                 `);
             });
 
+            $('#pokeStats > #title > h2').html("Estadísticas");
+
+            CanvasJS.addColorSet("pokePastels",
+                [
+
+                "#87CEEB",
+                "#F08080",
+                "#98FB98",
+                "#FF1493",
+                "#00FA9A",
+                "#FFFF00"                
+                ]);
+
             let options = {
+                colorSet: "pokePastels", 
                 animationEnabled: true,
+                axisY: {
+                    title: "Puntaje",
+                    titleFontFamily: "IBM Plex Mono",
+                    labelFontFamily: "IBM Plex Mono"
+                    },
+                axisX: {
+                    title: "Categorías",
+                    titleFontFamily: "IBM Plex Mono",
+                    labelFontFamily: "IBM Plex Mono"
+                    },
                 legend: {
                     verticalAlign: "bottom",
                     horizontalAlign: "center"
+                    },
+                toolTip: {
+                    fontFamily: "IBM Plex Mono",
                     },
                 data: [              
                     {
                         type: "column",
                         dataPoints: [
-                            { label: "HP",  y: infoPoke.stats.stat  },
-                            { label: "Ataque", y: response.public_gists  },
-                            { label: "Defensa", y: response.followers  },
-                            { label: "Ataque Especial",  y: response.following  },
-                            { label: "Defensa Especial",  y: response.following  },
-                            { label: "Velocidad",  y: response.following  },
+                            { label: "HP",  y: infoPoke.stats[0].base_stat  },
+                            { label: "Ataque", y: infoPoke.stats[1].base_stat  },
+                            { label: "Defensa", y: infoPoke.stats[2].base_stat  },
+                            { label: "Ataque Especial",  y: infoPoke.stats[3].base_stat  },
+                            { label: "Defensa Especial",  y: infoPoke.stats[4].base_stat  },
+                            { label: "Velocidad",  y: infoPoke.stats[4].base_stat  },
                         ]
                     }
                 ]
             };
     
             $("#pokeGraph").CanvasJSChart(options);
-            $('#nombreUsuario').val("");
-            nombreUsuario.focus();
+
         },
 
         error: function(error){
@@ -79,4 +108,6 @@ $('#pokesearch').on('click',()=>{
               )
         }
     });
+
+    $('input').val("");
 });
